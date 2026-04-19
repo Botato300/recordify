30c1a648d526941426c8b788c8c3b4c5d844212d
Botato300 <bigvotati@gmail.com>
2026-04-18 23:58:51 -0300

    feat: agrega los archivos iniciales

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    function startRecording() {
+        console.log("comenzando!");
+    }
+
+    function stopRecording() {
+        console.log("detenido");
+    }
+</script>
+
+<h1>Recordify</h1>
+
+<button onclick={startRecording}>Grabar</button>
+<button onclick={stopRecording}>Detener</button>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { mount } from "svelte";
+
+import "./global.css";
+
+import App from "./App.svelte";
+
+const appRoot = document.getElementById("app");
+
+if (!appRoot)
+    throw new Error("No se pudo encontrar un elemento con el id 'app'.");
+
+const app = mount(App, { target: appRoot });
+
+export default app;
