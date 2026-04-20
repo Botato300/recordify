7999a9365349daaeaa9cb8cf65aa45aaebea86d0
Botato300 <bigvotati@gmail.com>
2026-04-19 23:54:26 -0300

    chore: add alias for imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import path from "node:path";
+
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	resolve: {
+		alias: {
+			"@": path.resolve(import.meta.dirname, "./src")
+		}
+	}
 });
